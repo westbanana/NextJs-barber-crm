c56be95e23730523e1de653695457c70d96d00dc
westbanana <westbanana4@gmail.com>
2024-03-20 03:27:07 +0200

    Fix work schedule col aggrid

--- src/constants/colDefs.ts
+++ src/constants/colDefs.ts
@@ -14,7 +14,17 @@ export const ColDefs:ColDef[] = [
     field: 'name', headerName: "Ім'я", tooltipField: "Ім'я",
   },
   { field: 'position', headerName: 'Посада', tooltipField: 'Посада' },
-  { field: 'work_schedule', headerName: 'Графік роботи', tooltipField: 'Графік роботи' },
+  {
+    field: 'work_schedule',
+    headerName: 'Графік роботи',
+    tooltipField: 'Графік роботи',
+    valueGetter: (params) => {
+      const { time, days } = params.data.work_schedule;
+      const timeResult = `${time.from}/${time.to}`;
+      const daysResult = days ? `${days?.from}-${days?.to}` : 'ПН-ПТ';
+      return `${daysResult} | ${timeResult}`;
+    },
+  },
   {
     field: '',
     sortable: false,
