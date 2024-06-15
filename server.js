c0067708e8019f7172abe6c2d4f4bb305e2e2d41
westbanana <westbanana4@gmail.com>
2024-06-15 04:11:35 +0300

    Changed animations, types.

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
-// JSON Server module
 const jsonServer = require('json-server');
 
-const server = jsonServer.create();
-const router = jsonServer.router('db/db.json');
+const path = require('path');
 
-// Make sure to use the default middleware
+const app = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, '../../test/api/dev.json'));
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-  // Add custom route here if needed
-  jsonServer.rewriter({
-    '/api/*': '/$1',
-  }),
-);
-server.use(router);
-// Listen to port
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+app.use((req, res, next) => {
+  setTimeout(next, 10000);
 });
+app.use(middlewares);
+app.use(router);
 
-// Export the Server API
-module.exports = server;
+server = app.listen(3000, () => {
+  console.log('JSON Server is running on localhost:3000');
+  done();
+});
