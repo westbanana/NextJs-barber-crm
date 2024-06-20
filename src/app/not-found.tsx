44e6f1c63f4f9e99a0660079ed47b2411588a6c3
westbanana <westbanana4@gmail.com>
2024-06-20 17:16:30 +0300

    Changed not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Link from 'next/link';
+
+import Page from '@components/ui/Page/Page';
+import animations from '@/variables/animations/animations.module.scss';
+import { PAGES_LINKS_INSTANCE } from '@config/links';
+import Button from '@components/ui/Button/Button';
+
+import cls from './style.module.scss';
 
 const ErrorPage = () => (
-  <div>
-    ERROR 404
-  </div>
+  <Page className={cls.errorPage}>
+    <span className={animations.growIn}>ERROR 404</span>
+    <Button>
+      <Link href={PAGES_LINKS_INSTANCE.HOME}>Home</Link>
+    </Button>
+  </Page>
 );
 
 export default ErrorPage;
