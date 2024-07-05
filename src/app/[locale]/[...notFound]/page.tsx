4946205c00e442285b81d1a9f346b8455740149b
Illia <93942346+westbanana@users.noreply.github.com>
2024-07-05 14:24:58 +0300

    Change not found page;

--- src/app/[locale]/[...notFound]/page.tsx
+++ src/app/[locale]/[...notFound]/page.tsx
@@ -6,20 +6,21 @@ import Page from '@components/ui/Page/Page';
 import animations from '@variables/animations/animations.module.scss';
 import { PAGES_LINKS_INSTANCE } from '@config/links';
 import Button from '@components/ui/Button/Button';
+import { classNames } from '@lib/classNames/classNames';
 
 import cls from '../style.module.scss';
 
 const ErrorPage = () => {
   const t = useTranslations();
   return (
-    <Page className={cls.errorPage}>
+    <Page className={cls.notFoundPage}>
       <span
-        className={animations.growIn}
+        className={classNames(cls.notFoundText, {}, [animations.growIn])}
       >
         {t('not-found-page.title')}
       </span>
       <Button className={cls.homeButton}>
-        <Link href={PAGES_LINKS_INSTANCE.HOME}>{t('buttons.home')}</Link>
+        <Link href={PAGES_LINKS_INSTANCE.HOME} replace>{t('buttons.home')}</Link>
       </Button>
     </Page>
   );
